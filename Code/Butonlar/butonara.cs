4a1cf4a5dd1387351d1f33ccf1f49158373c45d3
Podofko <53979150+Podofko@users.noreply.github.com>
2020-05-05 00:58:29 +0300

    Update butonara.cs

--- Code/Butonlar/butonara.cs
+++ Code/Butonlar/butonara.cs
@@ -1,13 +1,1 @@
-namespace Uygulama_1
-{
-        public Form1()
-        {
-            InitializeComponent();
-        }
 
-        private void buttonGit_click(object sender, EventArgs e)
-        {
-            webBrowser1.Navigate(textBox1.Text);
-        }
-    }
-}
